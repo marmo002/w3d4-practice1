fd37481b38b184cfe56d1935c33a874d732b74e9
marmo002 <marmo_02@hotmail.com>
2018-11-15 20:55:26 -0500

    Added Collector class which collect quotes and stores them in categories. Also created random mehod that takes a topic as argument and returns a random quote of that topic

--- 03-oop_inheritance-master/genius.rb
+++ 03-oop_inheritance-master/genius.rb
@@ -4,8 +4,39 @@ class MathGenius < Multilinguist
 
   def report_total(array)
     result = array.sum
-    message = "The total is: "
-    "#{ message } #{ result }"
+    if @current_lang == 'en'
+      message = "The total is"
+    else
+      message = self.say_in_local_language('The total is')
+    end
+
+    "#{ message }: #{ result }"
+  end
+
+end
+
+class Collector < Multilinguist
+
+  def initialize
+    super
+    @quotes = {
+      wisdom: [],
+      friendship: [],
+      Anniversary: [],
+    }
+  end
+
+  def quotes
+    @quotes
+  end
+
+  def add_quote(topic, quote)
+    @quotes[topic.to_sym] << quote
+  end
+
+  def random(topic)
+    text = @quotes[topic.to_sym].sample
+    self.say_in_local_language(text)
   end
 
 end
